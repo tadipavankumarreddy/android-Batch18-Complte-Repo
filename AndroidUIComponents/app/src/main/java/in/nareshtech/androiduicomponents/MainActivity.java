f57df1f60580c41a3c57b6d40f4073b3053db02c
Pavan Kumar Reddy Tadi <tadipavankumarreddy@gmail.com>
2023-06-08 20:31:47 +0530

    UI Components

--- AndroidUIComponents/app/src/main/java/in/nareshtech/androiduicomponents/MainActivity.java
+++ AndroidUIComponents/app/src/main/java/in/nareshtech/androiduicomponents/MainActivity.java
@@ -4,14 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.RadioGroup;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,20 +28,40 @@ public class MainActivity extends AppCompatActivity {
     private CheckBox english, french, german;
     private String gender = "";
 
+    private List<String> countries;
+    private Spinner spinner;
+
+    private String country = "";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        person_name = findViewById(R.id.person_name);
-        person_age = findViewById(R.id.person_age);
-        submit = findViewById(R.id.button);
-        textView = findViewById(R.id.result);
-        g = findViewById(R.id.gender);
-        english = findViewById(R.id.eng);
-        french = findViewById(R.id.fren);
+        intializeTheViews();
 
-        // Get the data as soon as an option is selected
+        intializeTheList();
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_1,countries);
+        spinner.setAdapter(adapter);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                // when the item is selected, we come to this method
+                if(parent.getItemAtPosition(position).toString().equals("SELECT YOUR COUNTRY")){
+                    Toast.makeText(MainActivity.this, "Please select your country", Toast.LENGTH_SHORT).show();
+                }else{
+                    country = parent.getItemAtPosition(position).toString();
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                // when nothing is selected we come here
+            }
+        });
+
         g.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(RadioGroup group, int checkedId) {
@@ -50,8 +76,27 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 String n = person_name.getText().toString();
-                int a = Integer.parseInt(person_age.getText().toString());
-                textView.setText(n+"\n"+a+"\n"+gender);
+                int a;
+                if(n.isEmpty()){
+                    person_name.setError("Please enter your name");
+                }
+                if(person_age.getText().toString().isEmpty()){
+                    a = 0;
+                    person_age.setError("Age cannot be null");
+                }else{
+                  a = Integer.parseInt(person_age.getText().toString());
+                }
+                String lang = "";
+                if(english.isChecked()){
+                    lang = lang+"English ";
+                }
+                if(german.isChecked()){
+                    lang += "German ";
+                }
+                if(french.isChecked()){
+                    lang+= "French ";
+                }
+                textView.setText(n+"\n"+a+"\n"+gender+"\n"+lang+"\n"+country);
 
                 //clear the contents of the TextInputEditText boxes
                 person_name.getText().clear();
@@ -59,5 +104,28 @@ public class MainActivity extends AppCompatActivity {
                 person_age.clearFocus();
             }
         });
+    }
+
+
+
+    private void intializeTheList() {
+        countries = new ArrayList<>();
+        countries.add("SELECT YOUR COUNTRY");
+        countries.add("INDIA");
+        countries.add("Pakistan");
+        countries.add("USA");
+        countries.add("Russia");
+    }
+
+    private void intializeTheViews() {
+        person_name = findViewById(R.id.person_name);
+        person_age = findViewById(R.id.person_age);
+        submit = findViewById(R.id.button);
+        textView = findViewById(R.id.result);
+        g = findViewById(R.id.gender);
+        english = findViewById(R.id.eng);
+        french = findViewById(R.id.fren);
+        german = findViewById(R.id.germ);
+        spinner = findViewById(R.id.spinner);
     }
 }
